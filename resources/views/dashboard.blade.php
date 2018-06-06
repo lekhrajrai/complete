da340cfdf6899ce51f072479a7277bf80b609c10
lekhrajrai <rae.lekhraj@gmail.cm>
2018-06-06 09:44:24 +0545

    to be merged

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,25 +1,6 @@
 @extends('layout.master')
 
 @section('content')
-
-   <!--  <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div> -->
-
     <div>
         <div class="btn btn-sm bg-success mt-5 ml-5" id="btnAddHubby" data-target="#custom-modal" data-toggle="modal">Add Hubby
         </div>  
